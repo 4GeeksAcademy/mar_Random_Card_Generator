57d9289411ac155941454b8556393443f056d93f
Mar 🌊 <mariajara929@gmail.com>
2024-09-12 23:14:37 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let valueList = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let iconList = ["♦", "♥", "♠", "♣"];
+  function getRandomItem(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  let randomValue = getRandomItem(valueList);
+  let randomIcon = getRandomItem(iconList);
+  let iconsDiv = document.querySelectorAll(".icon");
+  iconsDiv.forEach(value => (value.innerHTML = randomIcon));
+  let valueDiv = document.querySelector(".value");
+  valueDiv.innerText = randomValue;
 };
