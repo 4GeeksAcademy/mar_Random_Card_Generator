e1c95a9bff30343f380237e6a9587fea8733b223
Mar 🌊 <mariajara929@gmail.com>
2024-09-13 17:12:58 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -17,4 +17,28 @@ window.onload = function() {
   iconsDiv.forEach(value => (value.innerHTML = randomIcon));
   let valueDiv = document.querySelector(".value");
   valueDiv.innerText = randomValue;
+  if (randomIcon == "♦" || randomIcon == "♥") {
+    iconsDiv.forEach(value => (value.style.color = "red"));
+    valueDiv.style.color = "red";
+  }
+  let cardContainer = document.querySelector(".cardContainer");
+  let imageList = [
+    "src/Blonnopie_1.png",
+    "src/Blonnopie_2.png",
+    "src/Blonnopie_3.png",
+    "src/Blonnopie_4.png",
+    "src/Blonnopie_5.png",
+    "src/Blonnopie_6.png",
+    "src/Blonnopie_7.png",
+    "src/Blonnopie_8.png",
+    "src/Blonnopie_9.png",
+    "src/Blonnopie_10.png",
+    "src/Blonnopie_11.png",
+    "src/Blonnopie_12.png",
+    "src/Blonnopie_13.png",
+    "src/Blonnopie_14.png",
+    "src/Blonnopie_15.png"
+  ];
+  let randomImage = getRandomItem(imageList);
+  cardContainer.style.backgroundImage = `url('${randomImage}')`;
 };
